d40d1a2c2cc37fd89f46375ecfcd4644e53982c2
VariPor <116594957+VariPor@users.noreply.github.com>
2023-03-11 11:13:52 +0000

    sandbox

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
+#include <math.h>
 
-int main() { return 0; }
+int divCount (int n) {
+    for (int j = 2; j <= n / 5 + 1; ++j) {
+        if (n % static_cast<int>(pow(5, j)) != 0) return j - 1;
+    }
+    return n / 5;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    int res = 0;
+    for (int i = 5; i <= n; i += 5) {
+        res += divCount(i);
+    } 
+    std::cout << res;
+    return 0;
+}
