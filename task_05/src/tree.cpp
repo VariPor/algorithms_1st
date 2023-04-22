8a44013fcd5282dc1c0a178c9a3d1445204c0267
VariPor <116594957+VariPor@users.noreply.github.com>
2023-04-22 15:15:32 +0000

    without deleting and tests

--- task_05/src/tree.cpp
+++ task_05/src/tree.cpp
@@ -2,40 +2,145 @@
 
 #include "exception"
 
-struct Tree::Node {
-  std::shared_ptr<Node> parent = nullptr;
-  std::shared_ptr<Node> leftChild = nullptr;
-  std::shared_ptr<Node> rightChild = nullptr;
-  int value;
-  int key;
-  Node(int k, int v) : key{k}, value{v} {}
-};
+bool operator==(const Tree::Node &left, const Tree::Node &right) {
+  if (left.parent != right.parent) return false;
+  if (left.leftChild != right.leftChild) return false;
+  if (left.rightChild != right.rightChild) return false;
+  return true;
+}
+
+bool operator!=(const Tree::Node &left, const Tree::Node &right) {
+  return !(left == right);
+}
+
+Tree::Node::Node(Node &node)
+    : parent{node.parent},
+      leftChild{node.leftChild},
+      rightChild{node.rightChild},
+      key{node.key},
+      value{node.value} {}
 
 Tree::Tree() {}
 
+Tree::Node::Node(int k, int v) : key{k}, value{v} {}
+
 bool Tree::Insert(int key, int value) {
+  auto result = InsertWithoutSplay(key, value);
+  Splay(result.first);
+  return result.second;
+}
+
+std::pair<Tree::Node &, bool> Tree::InsertWithoutSplay(int key, int value) {
   if (root == nullptr) {
     root = std::shared_ptr<Node>(std::make_shared<Node>(Node(key, value)));
-    return true;
+    return std::pair<Tree::Node &, bool>(*(root.get()), true);
   }
-  if (root->key < key) return Tree(root->leftChild).Insert(key, value);
-  if (root->key > key) return Tree(root->rightChild).Insert(key, value);
-  if (root->key == key) return false;
+  if (root->key < key)
+    return (Tree(root->leftChild).InsertWithoutSplay(key, value));
+  if (root->key > key)
+    return Tree(root->rightChild).InsertWithoutSplay(key, value);
+  if (root->key == key)
+    return std::pair<Tree::Node &, bool>(*(root.get()), false);
 }
 
 void Tree::InsertOrUpdate(int key, int value) {
-  if (root == nullptr)
-    root = std::shared_ptr<Node>(std::make_shared<Node>(Node(key, value)));
-  if (root->key < key) Tree(root->leftChild).InsertOrUpdate(key, value);
-  if (root->key > key) Tree(root->rightChild).InsertOrUpdate(key, value);
-  if (root->key == key) root->value = value;
+  auto result = InsertOrUpdateWithoutSplay(key, value);
+  Splay(result);
 }
 
-int Tree::Find(int key) const {
+Tree::Node &Tree::InsertOrUpdateWithoutSplay(int key, int value) {
+  if (root == nullptr) {
+    root = std::shared_ptr<Node>(std::make_shared<Node>(Node(key, value)));
+    return *(root.get());
+  }
+  if (root->key < key)
+    return Tree(root->leftChild).InsertOrUpdateWithoutSplay(key, value);
+  if (root->key > key)
+    return Tree(root->rightChild).InsertOrUpdateWithoutSplay(key, value);
+  if (root->key == key) {
+    root->value = value;
+    return *(root.get());
+  }
+}
+
+int Tree::Find(int key) {
+  auto result = FindWithoutSplay(key);
+  Splay(result.first);
+  return result.second;
+}
+
+std::pair<Tree::Node &, int> Tree::FindWithoutSplay(int key) {
   if (root == nullptr) throw std::out_of_range("No such key\n");
   if (root->key < key) Tree(root->leftChild).Find(key);
   if (root->key > key) Tree(root->rightChild).Find(key);
-  if (root->key == key) return root->value;
+  if (root->key == key)
+    return std::pair<Node &, int>(*(root.get()), root->value);
 }
 
 void Tree::Delete(int key) {}
+
+void Tree::Zig(Node &element) {
+  if (!element.parent) return;
+  if (element == *(element.parent->leftChild.get())) {
+    auto old_parent = element.parent;
+    auto old_left_child = element.leftChild;
+    auto old_right_child = element.rightChild;
+    *(element.parent->parent) = element;
+    element.rightChild = element.parent;
+    element.parent = old_parent->parent;
+    *(old_parent->leftChild) = element;
+    old_parent->leftChild = old_right_child;
+    old_right_child->parent = old_parent;
+  }
+  if (element == *(element.parent->rightChild.get())) {
+    auto old_parent = element.parent;
+    auto old_right_child = element.rightChild;
+    auto old_left_child = element.leftChild;
+    *(element.parent->parent) = element;
+    element.leftChild = element.parent;
+    element.parent = old_parent->parent;
+    *(old_parent->rightChild) = element;
+    old_parent->rightChild = old_left_child;
+    old_left_child->parent = old_parent;
+  }
+}
+
+void Tree::ZigZig(Node &element) {
+  Zig(*element.parent);
+  Zig(element);
+}
+
+void Tree::ZigZag(Node &element) {
+  Zig(element);
+  Zig(element);
+}
+
+void Tree::Splay(Node &element) {
+  while (element != *(root.get())) {
+    if (this->HaveGrandpa(element) && IsDirectChild(element)) ZigZig(element);
+    if (this->HaveGrandpa(element) && !IsDirectChild(element)) ZigZag(element);
+    if (!(this->HaveGrandpa(element))) Zig(element);
+  }
+}
+
+bool Tree::IsDirectChild(const Node &element) {
+  if (IsLeftChild(element) && IsLeftChild(*(element.parent))) return true;
+  if (IsRightChild(element) && IsRightChild(*(element.parent))) return true;
+  return false;
+}
+
+bool Tree::IsLeftChild(const Node &element) {
+  if (element == *(element.parent->leftChild.get())) return true;
+  return false;
+}
+
+bool Tree::IsRightChild(const Node &element) {
+  if (element == *(element.parent->rightChild.get())) return true;
+  return false;
+}
+
+bool Tree::HaveGrandpa(const Node &element) {
+  if (element.parent == nullptr) return false;
+  if (element.parent->parent != nullptr) return true;
+  return false;
+}
--- task_05/src/tree.hpp
+++ task_05/src/tree.hpp
@@ -2,7 +2,31 @@
 #include <memory>
 
 class Tree {
-  struct Node;
+ public:
+  struct Node {
+    std::shared_ptr<Node> parent = nullptr;
+    std::shared_ptr<Node> leftChild = nullptr;
+    std::shared_ptr<Node> rightChild = nullptr;
+    int value;
+    int key;
+    Node(int k, int v);
+    Node(Node& node);
+
+    Node(const Node& other) = default;
+    Node& operator=(const Node& rhs) = default;
+
+    Node(Node&& other) = default;
+    Node& operator=(Node&& rhs) = default;
+  };
+
+ private:
+  void Zig(Node& element);
+  void ZigZig(Node& element);
+  void ZigZag(Node& element);
+  void Splay(Node& element);
+  std::pair<Node&, bool> InsertWithoutSplay(int key, int value);
+  Node& InsertOrUpdateWithoutSplay(int key, int value);
+  std::pair<Node&, int> FindWithoutSplay(int key);
 
  public:
   std::shared_ptr<Node> root = nullptr;
@@ -11,6 +35,11 @@ class Tree {
 
   bool Insert(int key, int value);
   void InsertOrUpdate(int key, int value);
-  int Find(int key) const;
+  int Find(int key);
   void Delete(int key);
+
+  bool IsLeftChild(const Node& element);
+  bool IsRightChild(const Node& element);
+  bool IsDirectChild(const Node& element);
+  bool HaveGrandpa(const Node& element);
 };
