98645aca21532679e2141562971c011dd3fc1b2e
VariPor <116594957+VariPor@users.noreply.github.com>
2023-06-21 08:58:12 +0000

    correction

--- task_03/src/sort.cpp
+++ task_03/src/sort.cpp
@@ -10,6 +10,7 @@ std::vector<int> Sort(const std::vector<int>& input) {
   std::vector<int> bigger_part(input.begin() + middle, input.end());
 
   bigger_part = Sort(bigger_part);
+  smaller_part = Sort(smaller_part);
   for (size_t i = 0, j = 0;
        i < smaller_part.size() || j < bigger_part.size();) {
     if (smaller_part[i] <= bigger_part[j]) {
